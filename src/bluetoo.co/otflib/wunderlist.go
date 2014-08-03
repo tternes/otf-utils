5798336ec7e174c7202ac743f546285a6fcf6165
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-08-03 14:36:28 -0500

    Wunderlist export

--- src/bluetoo.co/otflib/wunderlist.go
+++ src/bluetoo.co/otflib/wunderlist.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"errors"
 	"io/ioutil"
+	"sort"
 )
 
 var wunderlistApiUrl string = "https://api.wunderlist.com"
@@ -13,7 +14,7 @@ var wunderlistApiUrl string = "https://api.wunderlist.com"
 // --------------------------------------------------
 
 type WunderlistAccount struct {
-	Account Account
+	account Account
 	wusername string
 	wpassword string
 	wtoken string
@@ -23,32 +24,115 @@ type WunderlistLoginResponse struct {
 	Token string
 }
 
+// --------------------------------------------------
+// Lists
+// --------------------------------------------------
+type WunderlistList struct {
+	Id string
+	Title string
+	Position float64
+}
+type WunderlistLists []WunderlistList
+func (items WunderlistLists) Len() int {
+	return len(items)
+}
+
+func (items WunderlistLists) Less(i, j int) bool {
+	return items[i].Position < items[j].Position
+}
+
+func (items WunderlistLists) Swap(i, j int) {
+	items[i], items[j] = items[j], items[i]
+}
+
+// --------------------------------------------------
+// Tasks
+// --------------------------------------------------
+type WunderlistTask struct {
+	Title string
+	Position float64
+	List_id string
+	Starred bool
+	Completed_at string // date
+	Deleted_at string // date
+}
+type WunderlistTasks []WunderlistTask
+
+func (items WunderlistTasks) Len() int {
+	return len(items)
+}
+
+func (items WunderlistTasks) Less(i, j int) bool {
+	return items[i].Position < items[j].Position
+}
+
+func (items WunderlistTasks) Swap(i, j int) {
+	items[i], items[j] = items[j], items[i]
+}
+
+// --------------------------------------------------
 func NewWunderlistAccount(username string, password string) *WunderlistAccount {
 	return &WunderlistAccount{ wusername: username, wpassword: password}
 }
 
 // interface method
-func (w WunderlistAccount) LoadServiceAccount() bool {
+func (w *WunderlistAccount) LoadServiceAccount() bool {
 
-	if(requestWunderlistToken(&w) != nil) {
+	if(requestWunderlistToken(w) != nil) {
 		fmt.Println("Failed to retrieve token")
 		return false
 	}
 
+	// TODO: goroutine?
 	// token available, request lists
-	if(requestWunderlistLists(&w) != nil) {
-		fmt.Println("Failed to retrieve lists")
+	lists, listError := requestWunderlistLists(w)
+	if(listError != nil) {
+		fmt.Println("Failed to retrieve lists", listError)
 		return false
 	}
 	
-	// foreach list, request tasks
+	// request tasks
+	tasks, taskError := requestWunderlistTasks(w)
+	if(taskError != nil) {
+		fmt.Println("Failed to retrieve tasks", taskError)
+		return false
+	}
+	
+	sort.Sort(lists)
+	sort.Sort(tasks)
+
+	// for _,list := range lists {
+	// 	// AddList(NewList(list.Title), w.GetAccount())
+	// 	fmt.Println(task)
+	// }
+	
+	// Merge lists together
+	account := w.GetAccount()
+	for _,wlist := range *lists {
+
+		list := NewList(wlist.Title)
+		
+		// locate items matching current project
+		for _,wtask := range *tasks {
+			
+			if(wtask.List_id == wlist.Id) {
+				task := NewTask(wtask.Title)
+				task.SetStarred(wtask.Starred)
+				task.SetCompleted(len(wtask.Completed_at) > 0)
+				AddTask(task, list)
+			}
+
+		}
+
+		AddList(list, account)
+	}
 	
 	return true
 }
 
 // interface method
 func (w *WunderlistAccount) GetAccount() *Account {
-	return &(w.Account)
+	return &(w.account)
 }
 
 // --------------------------------------------------
@@ -80,10 +164,59 @@ func requestWunderlistToken(w *WunderlistAccount) error {
 // Lists
 // --------------------------------------------------
 
-func requestWunderlistLists(w *WunderlistAccount) error {
-	return nil	
+func requestWunderlistLists(w *WunderlistAccount) (*WunderlistLists, error) {
+	url := wunderlistApiUrl + fmt.Sprintf("/me/lists")
+	
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", w.wtoken))
+	req.Header.Set("Content-Type", "text/json")
+	resp, _ := client.Do(req)
+	
+	fmt.Println(resp.Status)
+	
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	switch resp.StatusCode {
+	case 200:
+	default:
+		return nil, errors.New("invalid http response code")
+	}
+	
+	var listsResponse WunderlistLists
+	json.Unmarshal(body, &listsResponse)
+	
+	return &listsResponse, nil
 }
 
 // --------------------------------------------------
 // Tasks
 // --------------------------------------------------
+func requestWunderlistTasks(w *WunderlistAccount) (*WunderlistTasks,error) {
+	url := wunderlistApiUrl + fmt.Sprintf("/me/tasks")
+	
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", w.wtoken))
+	req.Header.Set("Content-Type", "text/json")
+	resp, _ := client.Do(req)
+	
+	fmt.Println(resp.Status)
+	
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	switch resp.StatusCode {
+	case 200:
+	default:
+		return nil, errors.New("invalid http response code")
+	}
+	
+	var tasksResponse WunderlistTasks
+	json.Unmarshal(body, &tasksResponse)
+	
+	return &tasksResponse, nil
+}
