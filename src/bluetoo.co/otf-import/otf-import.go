a3f397585420b7e9dc373118850e724c3c6281ef
Thaddeus Ternes <thaddeus@bluetoo.co>
2014-08-06 23:44:41 -0500

    Import support, tests

--- src/bluetoo.co/otf-import/otf-import.go
+++ src/bluetoo.co/otf-import/otf-import.go
@@ -1,17 +1,87 @@
 package main
 
-// import (
-// 	"flag"
-// 	"fmt"
-// )
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"unicode/utf8"
+	"bluetoo.co/otflib"
+)
+var service string
+var username string
+var password string
+var inputPath string
+var purgeRemoteData bool
+
+func init() {
+
+	purgeRemoteData = false
+	flag.StringVar(&service, "service", "", "Service name (e.g. \"wunderlist\" or \"todoist\")")
+	flag.StringVar(&username, "username", "", "Username / email for specified service")
+	flag.StringVar(&password, "password", "", "Password for specified service")
+	flag.StringVar(&inputPath, "input", service+".txt", "Path for exported file")
+}
+
+func exitUsage(message string) {
+	fmt.Println("Usage", message)
+	// flag.PrintDefaults()
+	// os.Exit(-1);
+}
 
 func main() {
-	// s := flag.Bool("s", false, "Server")
-	// flag.Parse()
-	//
-	// if *s {
-	// 	fmt.Println("server");
-	// } else {
-	// 	fmt.Println("something else")
-	// }
+	otfImport()
+}
+func otfImport() error {
+	// parameters
+	flag.Parse()
+	
+	var account otflib.Service;
+	
+	// validation
+	switch service {
+	case "wunderlist":
+		account = otflib.NewWunderlistAccount(username, password)
+	case "todoist":
+		account = otflib.NewTodoistAccount(username, password)
+	default:
+		exitUsage("invalid service")
+		return errors.New("Invalid service")
+	}
+
+	if(utf8.RuneCountInString(username) == 0 || utf8.RuneCountInString(password) == 0) {
+		exitUsage("username / password required")
+		return errors.New("Username and password required")
+	}
+
+	// load and purge the account
+	// load account
+	if(!account.LoadServiceAccount()) {
+		return errors.New("Failed to load service account")
+	}
+	
+	if(purgeRemoteData) {
+		remoteAccount := account.GetAccount()
+		for _,list := range remoteAccount.Lists {
+			err := account.RemoveListFromService(list)
+			if(err != nil) {
+				return err
+			}
+		}
+	}
+
+	// load local data
+	otfAccount := otflib.NewAccount()
+	err := otflib.ImportFromFile(otfAccount, inputPath)
+	if(err != nil) {
+		return err
+	}
+	
+	for _,list := range otfAccount.Lists {
+		err = account.AddListToService(list)
+		if(err != nil) {
+			return err
+		}
+	}
+	
+	return nil
 }
--- src/bluetoo.co/otf-import/otf-import_test.go
+++ src/bluetoo.co/otf-import/otf-import_test.go
@@ -2,8 +2,96 @@ package main
 
 import (
 	"testing"
+	"io/ioutil"
+	"os"
 )
 
-func TestPlaceholder(t *testing.T) {
+var tempdir string
+var usernameVar string
+var passwordVar string
+func init() {
+	tempdir,_ = ioutil.TempDir("", "TestAccountImport")
+}
+
+// ------------------------------------------------
+
+func TestWunderlistImport(t *testing.T) {
+
+	// usernameVar = "WUNDERLIST_USERNAME"
+	// passwordVar = "WUNDERLIST_PASSWORD"
+	// service = "wunderlist"
+	// inputPath = tempdir + "/wunderlist_test.txt"
+	//
+	// commonImportTest(t)
+}
+
+func TestTodoistExport(t *testing.T) {
+
+	usernameVar = "TODOIST_USERNAME"
+	passwordVar = "TODOIST_PASSWORD"
+	service = "todoist"
+	inputPath = tempdir + "/todoist_test.txt"
+
+	commonImportTest(t)
+}
+
+// ------------------------------------------------
+
+func commonImportTest(t *testing.T) {
+	username = os.Getenv(usernameVar)
+	password = os.Getenv(passwordVar)
 	
+	t.Logf("Input path (contents generated if missing): %s", inputPath)
+	t.Logf("credentials from environment: '%s' / '%s'", username, password)
+	
+	if(len(username) == 0 || len(password) == 0) {
+		t.Skip("Skipping test - required username/password missing")
+		return
+	}
+	
+	// input file exists?
+	if _, err := os.Stat(inputPath); os.IsNotExist(err) {
+		// generate a file
+		// write a temporary file
+		t.Logf("Creating temporary file %s", inputPath)
+		contents := []byte(`
+			= Have Five =
+			[ ] One
+			[ ] Two
+			[ ] Three
+			[ ] Four
+			[ ] Five
+			= Have Five More =
+			[ ] ★ One is important
+			[ ] Two
+			[ ] Three
+			[ ] Four
+			[ ] Five
+			= Have Twelve =
+			[ ] 1
+			[ ] 2
+			[ ] 3
+			[ ] 4
+			[ ] 5
+			[ ] 6
+			[ ] 7
+			[ ] 8
+			[ ] 9
+			[ ] 10
+			[ ] 11
+			[ ] 12
+			`)
+		err := ioutil.WriteFile(inputPath, contents, 0644)
+		if(err != nil) {
+			t.Errorf("Failed to write temp file", err)
+		}
+	}
+
+	if(otfImport() == nil) {
+		// ... thumbs up ...
+	} else {
+		t.Error("failed to import account")
+	}
+
+	// TODO: load account back from service and verify
 }
